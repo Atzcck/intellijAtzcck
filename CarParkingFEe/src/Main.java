da420da7ee004c2da6454124215c924519f4f097
Atzcck <82867179+Atzcck@users.noreply.github.com>
2022-07-18 09:50:56 +0300

    CarParkingFEe

---
+++ CarParkingFEe/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args)  {
+        carFee();
+    }
+
+    private static void carFee() {
+        int hoursStayed;
+        Scanner myScanner = new Scanner(System.in);
+
+
+
+    }
+}
